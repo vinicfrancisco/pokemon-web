ac4f8093b5460afc92423ddab3d857740b49d423
vinicfrancisco <vini.ibi@gmail.com>
2020-07-22 08:31:38 -0300

    added browser router on app.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from '~/pages/Home';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} exact />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+    </Switch>
   );
 }
