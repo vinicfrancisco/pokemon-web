517dd592e42ea78ec4081b8459d8ee5cebd27ffc
vinicfrancisco <vini.ibi@gmail.com>
2020-07-22 09:42:10 -0300

    added home, detail and edit routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Detail from '~/pages/Detail';
+import Edit from '~/pages/Edit';
 import Home from '~/pages/Home';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" component={Home} exact />
+      <Route path="/pokemon/:id" component={Detail} exact />
+      <Route path="/pokemon/:id/edit" component={Edit} exact />
     </Switch>
   );
 }
