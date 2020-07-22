7b5fb3c04af4c780a7c8b2e62a5c9ca593e17cec
vinicfrancisco <vini.ibi@gmail.com>
2020-07-22 17:10:07 -0300

    added redux provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import store from './store';
 import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </Provider>
 
       <GlobalStyle />
     </>
